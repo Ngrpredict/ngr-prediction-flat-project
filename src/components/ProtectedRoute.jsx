60b001afa60859ea9bec1e82841ce7ba875982b5
ngr-predictions-frontend-1 <mdquizcorner524@gmail.com>
2025-04-12 09:56:01 +0200

    ProtectedRoute.jsx

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { auth } from '../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+const ProtectedRoute = ({ children }) => {
+  const [user, loading] = useAuthState(auth);
+
+  if (loading) return <p className="text-center p-4">Loading...</p>;
+  if (!user) return <Navigate to="/login" replace />;
+  
+  return children;
+};
+
+export default ProtectedRoute;
